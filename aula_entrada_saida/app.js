12d082e224268249ef12d6a546184a2b151f749e
maria <marieduarda.menezes@gmail.com>
2025-11-19 23:49:07 -0300

    aula entrada e saida de dados

---
+++ aula_entrada_saida/app.js
@@ -0,0 +1,7 @@
+// comando de saida
+console.log("ola");
+
+//comando de saida
+const leia = require ('readline-sync');
+let nome = leia.question("qual o seu nome ?");
+console.log("ola   "  +  nome);
